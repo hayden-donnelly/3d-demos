8df4aa6a209f18b46c9f362ef6cedb79b4a56e7b
hayden-donnelly <hayden.git@proton.me>
2023-12-27 21:10:56 -0500

    Setup nixdev shell with gcc9 and glfw.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+    description = "3D graphics demos with OpenGL.";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/23.11";
+        flake-utils.url = "github:numtide/flake-utils";
+        nixgl = {
+            url = "github:guibou/nixgl";
+            inputs = {
+                nixpkgs.follows = "nixpkgs";
+                flake-utils.follows = "flake-utils";
+            };
+        };
+    };
+    outputs = inputs@{ self, nixpkgs, flake-utils, ... }: 
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: let
+        inherit (nixpkgs) lib;
+        pkgs = import nixpkgs { inherit system; };
+    in {
+        devShells = {
+            default = pkgs.mkShell {
+                name = "opengl";
+                buildInputs = with pkgs; [
+                    gcc9
+                    glfw
+                ];
+            };
+        };
+    });
+}
