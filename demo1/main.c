685e217fe79652095d3a74b236128f5e9fab6859
hayden-donnelly <hayden.git@proton.me>
2023-12-31 17:58:57 -0500

    Basic triangle drawing.

--- demo1/main.c
+++ demo1/main.c
@@ -1,6 +1,7 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <math.h>
 
 const int WIDTH = 1280;
 const int HEIGHT = 720;
@@ -20,6 +21,21 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main() 
 {
+    const char *vertex_shader_source = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+    const char *fragement_shader_source = 
+    "out vec4 FragColor;\n";
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+    "}\n\0";
+
     if (glfwInit() == GLFW_FALSE) 
     {
         printf("Failed to intialize glfw\n");
@@ -45,11 +61,58 @@ int main()
     }
     printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    GLuint fragement_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragement_shader, 1, &fragement_shader_source, NULL);
+    glCompileShader(fragement_shader);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragement_shader);
+
+    glLinkProgram(shader_program);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragement_shader);
+
+    GLfloat vertices[] = 
+    {
+        -0.5f, -0.5f * (float)(sqrt(3)) / 3.0f, 0.0f,
+        0.5f, -0.5f * (float)(sqrt(3)) / 3.0f, 0.0f,
+        0.0f, 0.5f * (float)(sqrt(3)) * 2.0f / 4.0f, 0.0f
+    };
+
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 3, (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     while(!glfwWindowShouldClose(window))
     {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteShader(shader_program);
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
