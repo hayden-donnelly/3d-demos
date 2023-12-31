7d8a68b7a790020b1e370522e1fa84c42f50c348
hayden-donnelly <hayden.git@proton.me>
2023-12-31 17:15:22 -0500

    Added window closing logic.

--- demo1/main.c
+++ demo1/main.c
@@ -5,6 +5,19 @@
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
 int main() 
 {
     if (glfwInit() == GLFW_FALSE) 
@@ -12,6 +25,8 @@ int main()
         printf("Failed to intialize glfw\n");
         return -1;
     }
+    glfwSetErrorCallback(error_callback);
+
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
     if(window == NULL)
     {
@@ -19,14 +34,23 @@ int main()
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
 
     int version = gladLoadGL(glfwGetProcAddress);
-    if (version == 0) {
+    if(version == 0) 
+    {
         printf("Failed to initialize OpenGL context\n");
         return -1;
     }
-
     printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
